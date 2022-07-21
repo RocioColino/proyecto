76c757d54f3c24f772ffd863511a569c46e24974
RocioColino <rcolino.tr@gmail.com>
2022-07-21 14:22:37 +0900

    cree las clases

--- ProyectoFinal/ProyectoFinal/BlogCorea/models.py
+++ ProyectoFinal/ProyectoFinal/BlogCorea/models.py
@@ -9,4 +9,15 @@ class Blogs(models.Model):
     cuerpo=models.CharField(max_length=900)
 
 class Cafes(models.Model):
-    
+    nombre=models.CharField(max_length=30)
+    precio=models.CharField(max_length=10)
+    imagen=models.ImageField()
+    direccion=models.CharField(max_length=40)
+    telefono=models.IntegerField()
+
+class Lugares(models.Model):
+    nombre=models.CharField(max_length=30)
+    imagen=models.ImageField()
+    descripcion=models.CharField(max_length=200)
+    direccion=models.CharField(max_length=50)
+    telefono=models.IntegerField()
